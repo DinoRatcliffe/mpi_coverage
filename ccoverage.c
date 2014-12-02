aaead8d9539bb3de00e78151ba8940d5fb6ab12f
Dino Ratcliffe <yo@dino.io>
2014-12-02 04:32:11 +0000

    Started distributed computation using mpi

--- ccoverage.c
+++ ccoverage.c
@@ -8,11 +8,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <mpi.h>
 
-#define MAXFN   200
-#define MAXLINE 2000
-#define MAXLOCS 200000
+#define MAXFN       200
+#define MAXLINE     2000
+#define MAXLOCS     200000
 
+#define JOBTAG      0
+#define RESULTTAG   1
+#define QUITTAG     2
+#define ROOTRANK    0
+
+//globals
+char *opnames[] = {"ebr", "ibr", "mser", "sfop"};
+int nopnames = 4;
+char *imsets[] = {"bark", "bikes", "boat", "graf", "leuv", "trees", "ubc",
+    "wall"};
+int nimsets = 8;
+
+//mpi globals
+int my_id, ierr, num_procs, running;
+    
 int load_locations (char *fn, float locs[][2], int first)
 {
     char line[MAXLINE], *kwd;
@@ -73,79 +89,130 @@ void coverage (float locs[][2], int nlocs, float *result, int *npaths, int *ncoi
     *ncoin = nc;
 }
 
-float mean_coverage(char **opnames, int nopnames, char**imsets, int nimsets)
+float mean_coverage(int *opindex, int nopindex)
 {
     int nfiles = 0, i, j, fno, nlocs, np, nc;
     float locs[MAXLOCS][2];
     float csum = 0.0, cov;
-    char fns[nopnames][MAXFN];
+    char fns[nopindex][MAXFN];
 
     for (i = 0; i < nimsets; i++) {
         for (fno = 1; fno < 7; fno++) {
-            nlocs = 0;
-            for (j = 0; j < nopnames; j++) {
-                sprintf(fns[j], "data/%s_%s%d.txt", opnames[j], imsets[i], fno);
-                nlocs = load_locations(fns[j], locs, nlocs); 
-            }
-            coverage(locs, nlocs, &cov, &np, &nc);
-            csum += cov;
+            int params[7] = {opindex[0], opindex[1], opindex[2], opindex[3], nopindex, i, fno};
+            MPI_Request request;
+            int proc = (rand() % (num_procs-1)) + 1;
+            //printf("sending: %i\n", proc);
+            MPI_Isend(&params, 7, MPI_INT, proc, JOBTAG, MPI_COMM_WORLD, &request);
             nfiles++;
         }
+    }
+
+    int message_num;
+    for(message_num = 0; message_num < nimsets*6; message_num++) {
+        MPI_Status status;
+        float cov;
+        MPI_Recv(&cov, 1, MPI_FLOAT, MPI_ANY_SOURCE, RESULTTAG, MPI_COMM_WORLD, &status);
+        //printf("recieved: %i\n", status.MPI_SOURCE);
+        csum += cov; 
     }
 
     return csum / nfiles;
 }
 
+void handle_coverage_job(int *opindex, int nopindex, int i, int fno) {
+    int nlocs = 0, np, nc, j;
+    float cov;
+    float locs[MAXLOCS][2];
+    char fns[nopindex][MAXFN];
+
+    for (j = 0; j < nopindex; j++) {
+       sprintf(fns[j], "data/%s_%s%d.txt", opnames[opindex[j]], imsets[i], fno);
+       nlocs = load_locations(fns[j], locs, nlocs);
+    }
+    coverage(locs, nlocs, &cov, &np, &nc);
+            
+    MPI_Request request;
+    MPI_Isend(&cov, 1, MPI_FLOAT, ROOTRANK, RESULTTAG, MPI_COMM_WORLD, &request);
+}
+
+void handle_message() {
+    MPI_Status status;
+    int params[7];
+    MPI_Recv(&params, 7, MPI_INT, ROOTRANK, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    switch(status.MPI_TAG) {
+        case JOBTAG:
+            handle_coverage_job(params, params[4], params[5], params[6]);
+            break;
+        case QUITTAG:
+            running = 0;
+            break;
+    }
+}
+
 int main (int argc, char **argv)
 {
-    char *opnames[] = {"ebr", "ibr", "mser", "sfop"};
-    int nopnames = 4;
-    char *imsets[] = {"bark", "bikes", "boat", "graf", "leuv", "trees", "ubc",
-        "wall"};
-    int nimsets = 8;
     int i1, i2, i3, i4;
     float mc;
 
-    // Calculate the coverage for each individual operator.
-    for (i1 = 0; i1 < nopnames; i1++) {
-        char *currentops[1] = {opnames[i1]};
-        mc = mean_coverage (currentops, 1, imsets, nimsets);
-        printf ("%s: %f\n", opnames[i1], mc);
-    }
+    ierr = MPI_Init(&argc, &argv);
+    ierr = MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+    ierr = MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
-    // Calculate the coverage for each combination of pairs of operators.
-    for (i1 = 0; i1 < nopnames; i1++) {
-        for (i2 = i1+1; i2 < nopnames; i2++) {
-            char *currentops[2] = {opnames[i1], opnames[i2]};
-            mc = mean_coverage (currentops, 2, imsets, nimsets);
-            printf ("%s + %s: %f\n", opnames[i1], opnames[i2], mc);
+    if (my_id == ROOTRANK) {
+        // Calculate the coverage for each individual operator.
+        for (i1 = 0; i1 < nopnames; i1++) {
+            int currentops[1] = {i1};
+            mc = mean_coverage (currentops, 1);
+            printf ("%s: %f\n", opnames[i1], mc);
         }
-    }
 
-    // Calculate the coverage for each combination of triplets of operators.
-    for (i1 = 0; i1 < nopnames; i1++) {
-        for (i2 = i1+1; i2 < nopnames; i2++) {
-            for (i3 = i2+1; i3 < nopnames; i3++) {
-                char *currentops[3] = {opnames[i1], opnames[i2], opnames[i3]};
-                mc = mean_coverage (currentops, 3, imsets, nimsets);
-                printf ("%s + %s + %s: %f\n", opnames[i1], opnames[i2], opnames[i3], mc);
+        // Calculate the coverage for each combination of pairs of operators.
+        for (i1 = 0; i1 < nopnames; i1++) {
+            for (i2 = i1+1; i2 < nopnames; i2++) {
+                int currentops[2] = {i1, i2};
+                mc = mean_coverage (currentops, 2);
+                printf ("%s + %s: %f\n", opnames[i1], opnames[i2], mc);
             }
         }
-    }
 
-    // Calculate the coverage for each combination of quadruplets of operators.
-    for (i1 = 0; i1 < nopnames; i1++) {
-        for (i2 = i1+1; i2 < nopnames; i2++) {
-            for (i3 = i2+1; i3 < nopnames; i3++) {
-                for (i4 = i3+1; i4 < nopnames; i4++) {
-                    char *currentops[4] = {opnames[i1], opnames[i2], opnames[i3], opnames[i4]};
-                    mc = mean_coverage (currentops, 4, imsets, nimsets);
-                    printf ("%s + %s + %s + %s: %f\n", opnames[i1], opnames[i2],
-                            opnames[i3], opnames[i4], mc);
+        // Calculate the coverage for each combination of triplets of operators.
+        for (i1 = 0; i1 < nopnames; i1++) {
+            for (i2 = i1+1; i2 < nopnames; i2++) {
+                for (i3 = i2+1; i3 < nopnames; i3++) {
+                    int currentops[3] = {i1, i2, i3};
+                    mc = mean_coverage (currentops, 3);
+                    printf ("%s + %s + %s: %f\n", opnames[i1], opnames[i2], opnames[i3], mc);
                 }
             }
         }
+
+        // Calculate the coverage for each combination of quadruplets of operators.
+        for (i1 = 0; i1 < nopnames; i1++) {
+            for (i2 = i1+1; i2 < nopnames; i2++) {
+                for (i3 = i2+1; i3 < nopnames; i3++) {
+                    for (i4 = i3+1; i4 < nopnames; i4++) {
+                        int currentops[4] = {i1, i2, i3, i4};
+                        mc = mean_coverage (currentops, 4);
+                        printf ("%s + %s + %s + %s: %f\n", opnames[i1], opnames[i2],
+                                opnames[i3], opnames[i4], mc);
+                    }
+                }
+            }
+        }
+
+        int current_proc;
+        MPI_Request request;
+        for (current_proc = 1; current_proc < num_procs; current_proc++) {
+            MPI_Isend(&current_proc, 1, MPI_INT, current_proc, QUITTAG, MPI_COMM_WORLD, &request);
+        }
+    } else {
+        running = 1;
+        while ( running ) {
+            handle_message();
+        }
     }
+
+    ierr = MPI_Finalize();
 
     return EXIT_SUCCESS;
 }
